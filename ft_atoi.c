77ac94d6647342c38ed3fc144af9d6e463691c7d
Daniel Palmer <dpalmer@c1r1p7.hive.fi>
2022-10-25 17:20:23 +0300

    atoi bullshit -> dealing with multiple overflow loops

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 08:04:37 by dpalmer           #+#    #+#             */
-/*   Updated: 2022/10/25 16:22:28 by dpalmer          ###   ########.fr       */
+/*   Updated: 2022/10/25 17:19:49 by dpalmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,10 @@ int	ft_atoi(const char *str)
 		sign = 1 - 2 * (str[i++] == '-');
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (sum == INT_MAX / 10 && str[i] == '8' && sign == -1
-			&& !ft_isdigit(str[i + 1]))
-			return (INT_MIN);
-		sum = 10 * sum + (str[i++] - '0');
+		// if (sum == INT_MAX / 10 && str[i] == '8' && sign == -1
+		// 	&& !ft_isdigit(str[i + 1]))
+		// 	return (INT_MIN);
+		sum = 10 * sum - (str[i++] - '0');
 	}
-	return (sum * sign);
+	return (-sum * sign);
 }
