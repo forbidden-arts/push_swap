501ff1fdd95860a7d5bba3412254aa5423efbb7e
Dan Palmer <dj.palmer81@gmail.com>
2022-10-25 11:03:55 +0300

    minor tweak to strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,23 +6,18 @@
 /*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 09:26:49 by dpalmer           #+#    #+#             */
-/*   Updated: 2022/10/25 09:42:51 by dpalmer          ###   ########.fr       */
+/*   Updated: 2022/10/25 11:01:13 by dpalmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-//	Can I change line 21 -> while (*(s++)) and eliminate line 25?
-
 char	*strchr(const char *s, int c)
 {
 	if (c == '/0')
 		return ((char *)s);
-	while (*s)
-	{
+	while (*(s++))
 		if (*s == c)
 			return ((char *)s);
-		s++;
-	}
 	return (0);
 }
