4134c62382586e3bd2ea66a24fb6fddf0cffd5d0
Dan Palmer <dpalmer@student.hive.fi>
2022-10-31 15:58:09 +0200

    checked lstdelone to check for content before deleting

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 18:22:28 by dpalmer           #+#    #+#             */
-/*   Updated: 2022/10/31 10:28:10 by dpalmer          ###   ########.fr       */
+/*   Updated: 2022/10/31 15:56:20 by dpalmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
 	if (!lst || !del)
 		return ;
-	(del)(lst->content);
+	if (lst->content)
+		(del)(lst->content);
 	free (lst);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 07:39:43 by dpalmer           #+#    #+#             */
-/*   Updated: 2022/10/31 15:43:20 by dpalmer          ###   ########.fr       */
+/*   Updated: 2022/10/31 15:57:16 by dpalmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	need_le = ft_strlen(needle);
 	if (!need_le || haystack == needle)
 		return (str);
+	if (!len)
+		return (NULL);
 	while (str[i] != '\0' && i < len)
 	{
 		j = 0;
