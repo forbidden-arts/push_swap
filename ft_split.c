f2b5320a6890678fdf34919420f4bab0ca47355b
Dan Palmer <dpalmer@student.hive.fi>
2022-11-02 18:35:30 +0200

    slight refactoring of split to clean up potential leaks on a malloc failure

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 16:38:00 by dpalmer           #+#    #+#             */
-/*   Updated: 2022/10/31 19:10:20 by dpalmer          ###   ########.fr       */
+/*   Updated: 2022/11/02 17:25:51 by dpalmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,8 +45,16 @@ static int	wrdlen(char *str, char c)
 	return (i);
 }
 
+static void	*split_undo(char **result, int index)
+{
+	while (index-- >= 0)
+		free(result[index]);
+	free(result);
+	return (NULL);
+}
+
 /* Notes for future improvement: To avoid the 25 line limit, this function
-could run in reverse and save at least two lines by dropping the iterator. 
+could run in reverse and save at least two lines by dropping the iterator.
 Could also move the allocation in to a separate function. */
 
 char	**ft_split(char const *s, char c)
@@ -70,7 +78,7 @@ char	**ft_split(char const *s, char c)
 			str++;
 		result[i - 1] = (char *)ft_calloc(wrdlen(str, c) + 1, sizeof(char));
 		if (!result[i - 1])
-			return (NULL);
+			return (split_undo(result, i - 2));
 		ft_strlcpy(result[i - 1], ft_substr(str, 0, wrdlen(str, c)),
 			wrdlen(str, c) + 1);
 		str += (wrdlen(str, c) + 1);
