3ee622ff570a8493d473dbbed6dd0da193696285
Dan Palmer <dj.palmer81@gmail.com>
2022-11-14 08:56:27 +0200

    fixed calloc overflow logic

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,21 @@
 /*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 08:31:02 by dpalmer           #+#    #+#             */
-/*   Updated: 2022/11/07 12:08:50 by dpalmer          ###   ########.fr       */
+/*   Updated: 2022/11/14 08:54:01 by dpalmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* ULONG_MAX is defined in the header */
+/* Make sure that count * size doesn't overflow, then malloc and zero it out. */
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
+	size_t	total;
 
-	if (count == ULONG_MAX - 1 && size == ULONG_MAX - 1)
+	total = count * size;
+	if (total / count != size)
 		return (NULL);
 	ptr = malloc(count * size);
 	if (!ptr)
